cf22567ba6d5b651bd3af9788972dfcc49ee5cd9
yinshengZ <zys2701999@gmail.com>
2024-02-27 00:58:51 +0000

    added function to get most frequently used and most quantity used inventories and registered there routes

--- app/Http/Controllers/InventoryStatController.php
+++ app/Http/Controllers/InventoryStatController.php
@@ -7,9 +7,9 @@ use DB;
 use Illuminate\Http\Request;
 use App\Models\Inventory;
 use App\Models\Treatment;
-
+use App\Services\Ultilities;
 use illuminate\Support\Carbon;
-
+use stdClass;
 
 class InventoryStatController extends Controller
 {
@@ -49,6 +49,88 @@ class InventoryStatController extends Controller
 
         return response()->json([
             'data' => $inventory,
+            'code' => 200
+        ]);
+    }
+
+    public function get_inventory_usage_counts($id, $year)
+    {
+
+        $inventory_count = DB::table('inventory_treatment')
+            ->select(DB::raw('inventory_id,COUNT(inventory_id) as counts, Month(updated_at) as month'))
+            ->where('inventory_id', '=', $id)
+            ->whereYear('updated_at', $year)
+            ->groupBy('month')
+            ->orderBy('month', 'ASC')
+            ->get();
+
+
+        $months_num = Ultilities::getAllMonths('num');
+        $final_data = [];
+
+        for ($i = 0; $i < count($months_num); $i++) {
+            $temp_data = new stdClass();
+            foreach ($inventory_count as $key => $count) {
+
+                if ($count->month == $months_num[$i]) {
+                    $temp_data->inventory_id = (int)$id;
+                    $temp_data->counts = $count->counts;
+                    $temp_data->month = $months_num[$i];
+                    break;
+                }
+            }
+            $tmp = (array)$temp_data;
+            if (empty($tmp)) {
+                $temp_data->inventory_id = (int)$id;
+                $temp_data->counts = 0;
+                $temp_data->month = $months_num[$i];
+                array_push($final_data, $temp_data);
+            } else {
+                array_push($final_data, $temp_data);
+            }
+        }
+
+        return response()->json([
+            'data' => $final_data,
+            'code' => 200
+        ]);
+    }
+
+    public function get_inventory_usage_units($id, $year)
+    {
+        $inventory_units = DB::table('inventory_treatment')
+            ->select(DB::raw('inventory_id,SUM(units) as units, Month(updated_at) as month'))
+            ->where('inventory_id', '=', $id)
+            ->whereYear('updated_at', $year)
+            ->groupBy('month')
+            ->orderBy('month', 'ASC')
+            ->get();
+        $months_num = Ultilities::getAllMonths('num');
+        $final_data = [];
+
+        for ($i = 0; $i < count($months_num); $i++) {
+            $temp_data = new stdClass();
+            foreach ($inventory_units as $key => $unit) {
+                if ($unit->month == $months_num[$i]) {
+                    $temp_data->inventory_id = (int)$id;
+                    $temp_data->units = $unit->units;
+                    $temp_data->month = $months_num[$i];
+                    break;
+                }
+            }
+            $tmp = (array)$temp_data;
+            if (empty($tmp)) {
+                $temp_data->inventory_id = (int)$id;
+                $temp_data->units = 0;
+                $temp_data->month = $months_num[$i];
+                array_push($final_data, $temp_data);
+            } else {
+                array_push($final_data, $temp_data);
+            }
+        }
+
+        return response()->json([
+            'data' => $final_data,
             'code' => 200
         ]);
     }
--- routes/api.php
+++ routes/api.php
@@ -140,6 +140,8 @@ Route::group([
 
     Route::get('inventory/expirying', [InventoryStatController::class, 'get_expirying_inventories']);
     Route::get('inventory/most_used', [InventoryStatController::class, 'most_used_inventories']);
+    Route::get('inventory/usage_counts/{id}/{year}', [InventoryStatController::class, 'get_inventory_usage_counts']);
+    Route::get('inventory/usage_units/{id}/{year}', [InventoryStatController::class, 'get_inventory_usage_units']);
     Route::get('inventory/most_quantity', [InventoryStatController::class, 'most_quantity_used']);
     Route::get('inventory/lowest_stocks', [InventoryStatController::class, 'lowest_stocks']);
 
